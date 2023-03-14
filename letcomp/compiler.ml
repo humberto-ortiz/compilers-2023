f13b5b4b2115040b876f94cdfe08abb9a817fdf0
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2023-03-14 10:32:02 -0400

    Update compiler for let and identifiers

--- letcomp/compiler.ml
+++ letcomp/compiler.ml
@@ -8,10 +8,12 @@ open Syntax
 
 type reg =
   | RAX
+  | RSP
 
 type arg =
   | Const of int64
   | Reg of reg
+  | RegOffset of reg * int
 
 type instruction =
   | IMov of arg * arg
@@ -20,11 +22,13 @@ type instruction =
 let reg_to_string (r : reg) : string =
   match r with
   | RAX -> "RAX"
+  | RSP -> "RSP"
 
 let arg_to_string ( a : arg ) : string =
   match a with
   | Const entero -> Int64.to_string entero
   | Reg r -> reg_to_string r
+  | RegOffset (r, o) -> "[" ^ (reg_to_string r) ^ " " ^ (string_of_int o) ^ "]"
 
 let instr_to_string (i : instruction) : string =
   match i with
@@ -37,15 +41,32 @@ let rec asm_to_string (asm : instruction list) : string =
   | [] -> ""
   | a :: instrs -> instr_to_string a ^ "\n" ^ asm_to_string instrs 
 
-let rec compile_expr (e : expr) : instruction list =
+type env = (string * int) list
+
+let rec lookup name env =
+  match env with
+  | [] -> failwith ("No se encontro " ^ name ^ "\n")
+  | (id, i)::rest -> if id = name then i else (lookup name rest) 
+
+let add name env =
+  let slot = 1 + (List.length env) in
+  ((name, slot)::env, slot)
+
+let rec compile_expr (e : expr) (env : env) : instruction list =
   match e with
   | Num n -> [ IMov (Reg RAX, Const n) ]
-  | Inc e -> compile_expr e @ [ IAdd (Reg RAX, Const 1L) ] 
-  | Dec e -> compile_expr e @ [ IAdd (Reg RAX, Const (-1L)) ]
+  | Inc e -> compile_expr e env @ [ IAdd (Reg RAX, Const 1L) ] 
+  | Dec e -> compile_expr e env @ [ IAdd (Reg RAX, Const (-1L)) ]
+  | Id x -> [ IMov (Reg RAX, RegOffset (RSP, ~-1 * 8 * (lookup x env) )) ]
+  | Let (x, i, d) -> 
+     let (env', pos) = add x env in
+     compile_expr i env @
+       [ IMov (RegOffset (RSP, ~-1 * 8 * pos) , Reg RAX) ] @
+         compile_expr d env'
   (* | _ -> failwith "No se compilar eso" *)
 
 let compile_prog (program : expr) : string =
-  let instrs = compile_expr program in
+  let instrs = compile_expr program [] in
   let asm_string = asm_to_string instrs in
   sprintf "
 section .text
